5a9cec3c6fe941133ef7189c975675bdf8bd1f08
jeantiston <jean.tiston.work@gmail.com>
2020-08-20 21:36:02 +0800

    Added nav links

--- src/components/nav.js
+++ src/components/nav.js
@@ -71,9 +71,9 @@ const Nav = () => {
 
                 <ul>
                     <li><a href="#about">About</a></li>
-                    <li><a href="#">Skills</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
                     <li><button>Download Resume</button></li>
                 </ul>
             </motion.nav>
