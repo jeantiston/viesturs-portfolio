f7f2927303e8f1335d445470c5acd0a93599222f
jeantiston <jean.tiston.work@gmail.com>
2020-09-04 17:44:33 +0800

    Made About responsive

--- src/sections/about.js
+++ src/sections/about.js
@@ -1,46 +1,28 @@
 import React from 'react'
 
 import SectionHeader from '@components/sectionheader'
+import aboutStyles from '@styles/about.module.css'
+
 import profilePhoto from '@assets/profile_photo.jpg'
 
 const About = () => {
-    const aboutStyles = {
-        color: "#F3C2D8",
-        margin: "25px"
-    }
-
-    const pStyles = {
-        marginBottom: "10px"
-    }
-
-    const photoStyle = {
-        borderRadius: "100px",
-        width: "200px",
-        height: "200px"
-    }
-
-    const wrapperStyle = {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center"
-    }
 
     return (
         <div id="about" style={{position: "relative"}}> 
         <SectionHeader name="Background" />
-            <div style={wrapperStyle}>
-                <div style={aboutStyles}>
-                    <p style={pStyles}>
+            <div className={aboutStyles.wrapperStyle}>
+                <div className={aboutStyles.about}>
+                    <p className={aboutStyles.aboutP}>
                         I’m a software engineer that focuses on creating beautiful and functional websites that clearly conveys your brand
                     </p>
-                    <p style={pStyles}>
+                    <p className={aboutStyles.aboutP}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisi scelerisque eu ultrices vitae auctor eu augue ut lectus. Vitae turpis massa sed elementum. 
                     </p>
-                    <p style={pStyles}>
+                    <p className={aboutStyles.aboutP}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt faucibus.
                     </p>
                 </div>
-                <img src={profilePhoto} alt="jean" style={photoStyle} />
+                <img src={profilePhoto} alt="jean" className={aboutStyles.photo} />
             </div>
         </div>
     )
