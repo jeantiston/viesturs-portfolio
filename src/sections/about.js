bf2fcbf40cacaa86095332f627e7c5603f87dd8e
jeantiston <jean.tiston.work@gmail.com>
2021-01-03 17:42:00 +0800

    Edited about description

--- src/sections/about.js
+++ src/sections/about.js
@@ -14,14 +14,15 @@ const About = () => {
             <div className={aboutStyles.wrapperStyle}>
                 <div className={aboutStyles.about}>
                     <p className={aboutStyles.aboutP}>
-                        I’m a software engineer that focuses on creating beautiful and functional websites that clearly conveys your brand
+                        I like working on visually appealing projects whether that’s a website, application interface, or marketing content. My ideal work is one that utilizes both my creative design and problem-solving skills.
                     </p>
                     <p className={aboutStyles.aboutP}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisi scelerisque eu ultrices vitae auctor eu augue ut lectus. Vitae turpis massa sed elementum. 
+                        After graduating from University of the Philippines with a Computer Engineering degree, I worked at FactSet Philippines as a Software Engineer for 3 years.
                     </p>
                     <p className={aboutStyles.aboutP}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt faucibus.
+                        I’m currently working as a freelance social media and campaign manager while continuously exercising my programming and web development skills.
                     </p>
+                    
                 </div>
                 <img src={profilePhoto} alt="jean" className={aboutStyles.photo} />
             </div>
