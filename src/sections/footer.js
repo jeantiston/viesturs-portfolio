d8270ce5e6d5d592d53481bf77196f431bee4095
jeantiston <jean.tiston.work@gmail.com>
2021-01-03 13:12:35 +0800

    Added social icons

--- src/sections/footer.js
+++ src/sections/footer.js
@@ -3,13 +3,24 @@ import React from 'react'
 import footerStyles from '@styles/footer.module.css'
 
 import mountainPhoto from '@assets/mountain-footer.png'
+import githubIcon from '@assets/038-github-white.svg'
+import linkedinIcon from '@assets/045-linkedin.svg'
+import twitterIcon from '@assets/043-twitter.svg'
+import instagramIcon from '@assets/025-instagram.svg'
+
+
 
 const Footer = () => {
     return (
         <div className={footerStyles.footerBg}>
             {/* <h1>Footer</h1> */}
             <img id="mountain-footer" src={mountainPhoto} />
-            <div className={footerStyles.icons}>icons</div> 
+            <div className={footerStyles.icons}>
+            <a href="https://github.com/jeantiston" target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="GitHub" className={footerStyles.footerIcon} /></a>
+            <a href="https://www.linkedin.com/in/jeantiston/" target="_blank" rel="noopener noreferrer"><img src={linkedinIcon} alt="LinkedIn" className={footerStyles.footerIcon} /></a>
+            <a href="https://www.instagram.com/jeloufish/" target="_blank" rel="noopener noreferrer"><img src={instagramIcon} alt="Instagram" className={footerStyles.footerIcon} /></a>
+            <a href="https://twitter.com/jeanlouisedev" target="_blank" rel="noopener noreferrer"><img src={twitterIcon} alt="Twitter" className={footerStyles.footerIcon} /></a>
+            </div> 
             <div className={footerStyles.credits}>
                 <div>
                     <p>Designed and built by:</p>
