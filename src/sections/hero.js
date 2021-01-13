8335c53244d10a8471de70983606cc78711abd37
jeantiston <jean.tiston.work@gmail.com>
2021-01-13 18:06:24 +0800

    Hero text animation

--- src/sections/hero.js
+++ src/sections/hero.js
@@ -8,15 +8,17 @@ import heroStyles from '@styles/hero.module.css'
 const heroVariants = {
     init: {
         opacity: 0,
-        x: '-25vw'
+        x: '-2vw'
     },
     visible: {
         opacity: 1,
         x: 0,
         transition: {
             type: 'tween',
-            delay: 0.5,
-            duration: 0.8
+            duration: 0.6,
+            ease: "easeIn",
+            delayChildren: 0.5,
+            staggerChildren: 0.3
         }
     }
 }
@@ -25,26 +27,31 @@ const Hero = () => {
 
     return (
         <div className={heroStyles.heroBg}>
-            <div className={heroStyles.hero}>
+            <motion.div className={heroStyles.hero}
+                variants={heroVariants}
+                initial="init"
+                animate="visible"
+            >
                 <div className={heroStyles.moonWrapper}>
                 </div>
                 
                 <motion.div className={heroStyles.headline}
                     variants={heroVariants}
-                    initial="init"
-                    animate="visible"
-
                 >
                     <p className={heroStyles.elevator} style={{textAlign: "left", fontSize: "clamp(1.2rem, 2vw, 1.5rem)", paddingLeft: "clamp(0.5rem, 2vw, 1rem)"}}>Hello, my name is</p>
                     <h1>Jean Tiston</h1>
                 </motion.div>
-                <div className={heroStyles.block}>
+                <motion.div className={heroStyles.block}
+                    variants={heroVariants}
+                >
                     <p className={heroStyles.elevator}>I’m a software engineer based in Manila focusing on creating stunning and functional websites, user interfaces and content that clearly reflects your brand. </p>
-                </div>
-                <div className={heroStyles.cta}>
+                </motion.div>
+                <motion.div className={heroStyles.cta}
+                    variants={heroVariants}
+                >
                     <Link to='/#contact' className={heroStyles.button} >Get In Touch</Link>                    
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
            
             
         </div>
