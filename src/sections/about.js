b1df070ac1ab2c0c4537815aa3772d8b835dbd81
jeantiston <jean.tiston.work@gmail.com>
2021-01-14 21:38:24 +0800

    Animation for about and skills

--- src/sections/about.js
+++ src/sections/about.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
+import { motion } from 'framer-motion'
+
 import SectionHeader from '@components/sectionheader'
 import aboutStyles from '@styles/about.module.css'
 
 import profilePhoto from '@assets/profile_photo.jpg'
+
+const aboutVariants = {
+    init: {
+        opacity: 0,
+        x: '-2vw'
+    },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            type: 'tween',
+            duration: 0.6,
+            ease: "easeIn",
+            delayChildren: 0.5,
+            staggerChildren: 0.3
+        }
+    }
+}
 
 const About = () => {
 
@@ -11,8 +31,14 @@ const About = () => {
         <div id="about" style={{position: "relative", zIndex: 4}}> 
         <SectionHeader name="Background" />
         <div className={aboutStyles.wrapwrap} >
-            <div className={aboutStyles.wrapperStyle}>
-                <div className={aboutStyles.about}>
+            <motion.div className={aboutStyles.wrapperStyle}
+                variants={aboutVariants}
+                initial="init"
+                animate="visible"
+            >
+                <motion.div className={aboutStyles.about}
+                    variants={aboutVariants}
+                >
                     <p className={aboutStyles.aboutP}>
                         I like working on visually appealing projects whether that’s a website, application interface, or marketing content. My ideal work is one that utilizes both my creative design and problem-solving skills.
                     </p>
@@ -23,9 +49,11 @@ const About = () => {
                         I’m currently working as a freelance social media and campaign manager while continuously exercising my programming and web development skills.
                     </p>
                     
-                </div>
-                <img src={profilePhoto} alt="jean" className={aboutStyles.photo} />
-            </div>
+                </motion.div>
+                <motion.img src={profilePhoto} alt="jean" className={aboutStyles.photo} 
+                    variants={aboutVariants}
+                />
+            </motion.div>
             </div>
         </div>
     )
--- src/sections/skills.js
+++ src/sections/skills.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { graphql, useStaticQuery } from "gatsby"
 
+import { motion } from 'framer-motion'
+
 import SectionHeader from '@components/sectionheader'
 import SkillsList from '@components/skillslist'
 
@@ -8,6 +10,24 @@ import skillStyle from '@styles/skills.module.css'
 
 // import devIcon from '@assets/development.svg'
 import resumeIcon from '@assets/resume.svg'
+
+const skillsVariants = {
+    init: {
+        opacity: 0,
+        x: '-2vw'
+    },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            type: 'tween',
+            duration: 0.6,
+            ease: "easeIn",
+            delayChildren: 0.5,
+            staggerChildren: 0.3
+        }
+    }
+}
 
 const Skills = () => {
 
@@ -42,12 +62,16 @@ const Skills = () => {
     return (
         <div id="skills" style={{position: "relative"}}>
             <SectionHeader name="Skills" />
-            <div className={skillStyle.listlist}>
+            <motion.div className={skillStyle.listlist}
+                variants={skillsVariants}
+                initial="init"
+                animate="visible"
+            >
                 { skillsHtml }
                 <a href="https://drive.google.com/file/d/1RTMy8t8rwMLprahJVLvMNgUiZPwPk3oE/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                     <SkillsList title="View My Resume" skills={[]} icon={resumeIcon} />
                 </a>
-            </div>
+            </motion.div>
         </div>
     )
 }
