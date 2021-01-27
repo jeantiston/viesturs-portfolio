c4416d715a18490f3fb098ee2fbefe413b6581fc
jeantiston <jean.tiston.work@gmail.com>
2021-01-27 14:11:07 +0800

    Added project animation

--- src/sections/projects.js
+++ src/sections/projects.js
@@ -1,30 +1,13 @@
 import React from 'react'
 import { graphql, useStaticQuery } from "gatsby"
 
-import { motion } from 'framer-motion'
+import Fade from 'react-reveal/Fade';
 
 import projectStyle from '@styles/project.module.css'
 
 import Card from '@components/card'
 import SectionHeader from '@components/sectionheader'
 
-const projectsVariants = {
-  init: {
-      opacity: 0,
-      x: '-2vw'
-  },
-  visible: {
-      opacity: 1,
-      x: 0,
-      transition: {
-          type: 'tween',
-          duration: 0.6,
-          ease: "easeIn",
-          delayChildren: 0.5,
-          staggerChildren: 0.3
-      }
-  }
-}
 
 const Projects = () => {
 
@@ -73,15 +56,14 @@ const Projects = () => {
     return (
         <div id="projects">
             <SectionHeader name="Projects" />
+            <Fade left>
+              <div className={projectStyle.cards}>
             
-            <motion.div className={projectStyle.cards} 
-              variants={projectsVariants}
-              initial="init"
-              animate="visible"
-            >
-                { projectsHtml }
-                
-            </motion.div>
+                  { projectsHtml }
+                  
+            
+              </div>
+              </Fade>
         </div>
 
     )
